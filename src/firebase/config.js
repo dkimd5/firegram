2a41cb8ebbe0758deb8a805a21decf887c56cd1f
dkimd5 <dkimd5@mail.ru>
2022-02-08 12:12:55 +0300

    firebase package added, config file created, firebase configs and imports added

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyCBfcUUSy3ifEybcNaaiuHGmHzv2FsM4",
+  authDomain: "kimd-firegram.firebaseapp.com",
+  projectId: "kimd-firegram",
+  storageBucket: "kimd-firegram.appspot.com",
+  messagingSenderId: "696295208301",
+  appId: "1:696295208301:web:2490dff08f3fc397bb9664",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const projectStorage = getStorage();
+const projectFirestore = getFirestore();
+
+export { projectStorage, projectFirestore };
