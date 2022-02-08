a417df10722b0cc1c08f7a67f09d2482cef6616b
dkimd5 <dkimd5@mail.ru>
2022-02-08 15:13:37 +0300

    visual progress line added

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
 function ProgressBar({ file, setFile }) {
   const { url, progress } = useStorage(file);
-  console.log(progress, url);
 
-  return <div className="progress-bar">progress</div>;
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 }
 
 export default ProgressBar;
