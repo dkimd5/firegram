7ef29c4e6e3babc9f5730bb9a7c7bfd6746b8bf1
dkimd5 <dkimd5@mail.ru>
2022-02-08 12:20:24 +0300

    change handler added for file

--- src/firebase/UploadForm.js
+++ src/firebase/UploadForm.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function UploadForm() {
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+    console.log(selected);
+  };
+
   return (
     <form>
-      <input type="file" />
+      <input type="file" onChange={changeHandler} />
     </form>
   );
 }
