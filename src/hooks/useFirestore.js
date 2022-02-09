6f389a24d25ab23c06604b0fdda5b37aad2a0f78
dkimd5 <dkimd5@mail.ru>
2022-02-09 12:43:53 +0300

    collection, query, orderBy, onSnapshot methods imported, code refactored

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,27 +1,26 @@
 import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 
-const useFirestore = (collection) => {
+const useFirestore = (collctnName) => {
   const [docs, setDocs] = useState([]);
 
   useEffect(() => {
-    first;
+    const collectionRef = collection(projectFirestore, collctnName);
+    const collectionRefOrdered = query(
+      collectionRef,
+      orderBy("createdAt", "desc")
+    );
+    const unsub = onSnapshot(collectionRefOrdered, (snap) => {
+      let documents = [];
+      snap.forEach((doc) => {
+        documents.push({ ...doc.data(), id: doc.id });
+      });
+      setDocs(documents);
+    });
 
-    return () => {
-      const unsub = projectFirestore
-        .collection(collection)
-        .orderBy("createdAt", "desc")
-        .onSnapshot((snap) => {
-          let documents = [];
-          snap.forEach((doc) => {
-            documents.push({ ...doc.data(), id: doc.id });
-          });
-          setDocs(documents);
-        });
-
-      return () => unsub();
-    };
-  }, [collection]);
+    return () => unsub();
+  }, [collctnName]);
 
   return { docs };
 };
