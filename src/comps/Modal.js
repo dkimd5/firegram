d03ab63d31351676422d4dafe74b88d79284774b
dkimd5 <dkimd5@mail.ru>
2022-02-09 16:48:22 +0300

    Pass setSelectedImg prop to Modal

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Modal({ selectedImg }) {
+function Modal({ selectedImg, setSelectedImg }) {
   return (
     <div className="backdrop">
       <img src={selectedImg} alt="enlarged img" />
