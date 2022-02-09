b3d0eb51fdcb53c21babb579b723a9f404be354a
dkimd5 <dkimd5@mail.ru>
2022-02-09 17:23:51 +0300

    Add initial, animate, transition motion props to img

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -17,7 +17,13 @@ function ImageGrid({ setSelectedImg }) {
             whileHover={{ opacity: 1 }}
             onClick={() => setSelectedImg(doc.url)}
           >
-            <img src={doc.url} alt="uploaded image" />
+            <motion.img
+              src={doc.url}
+              alt="uploaded image"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
           </motion.div>
         ))}
     </div>
