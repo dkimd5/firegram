7f70bd528e285f5d12b806eb10b138d29cfaee9c
dkimd5 <dkimd5@mail.ru>
2022-02-10 16:18:56 +0300

    Add delete image from firestore logic

--- src/hooks/useDeleteImg.js
+++ src/hooks/useDeleteImg.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { projectStorage, projectFirestore } from "../firebase/config";
 import { ref, deleteObject } from "firebase/storage";
+import { doc, deleteDoc } from "firebase/firestore";
 
 const useDeleteImg = (image) => {
   useEffect(() => {
@@ -9,6 +10,7 @@ const useDeleteImg = (image) => {
     const collectionRef = collection(projectFirestore, "images");
 
     deleteObject(deleteFileRef);
+    await deleteDoc(doc(projectFirestore, "images", image.name));
   }, [image]);
 };
 
